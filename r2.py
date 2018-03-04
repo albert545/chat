4bb19765b7c8fb74184d75b3ffe6b7af343d5b9c
Albert Liao <pulieden@gmail.com>
2018-03-04 11:27:18 +0800

    增加貼圖 圖片 計數

--- r2.py
+++ r2.py
@@ -11,19 +11,34 @@ def read_file(filename):
 def convert(lines):
 	person = None # 事先宣告 person的值為None
 	allen_word_count = 0
+	allen_sticker_count = 0
+	allen_image_count = 0
 	viki_word_count = 0
+	viki_sticker_count = 0
+	viki_image_count = 0	
+
 	for line in lines:
 		s = line.split(' ')  #分割清單
 		time = s[0]          #取出0 欄
 		name = s[1]
 		if name == 'Allen':
-			for m in s[2:]:
-				allen_word_count += len(m)
+			if s[2] == '貼圖':
+				allen_sticker_count += 1
+			elif s[2] == '圖片':
+				allen_image_count += 1
+			else:
+				for m in s[2:]:
+					allen_word_count += len(m)
 		if name == 'Viki':
-			for m in s[2:]:
-				viki_word_count += len(m)
-	print('allen say:' , allen_word_count)	
-	print('viki say:' , viki_word_count)
+			if s[2] == '貼圖':
+				viki_sticker_count += 1
+			elif s[2] == '圖片':
+				viki_image_count += 1
+			else:
+				for m in s[2:]:
+					viki_word_count += len(m)
+	print('allen say:' , allen_word_count,'貼圖:',allen_sticker_count,'圖片:',allen_image_count)	
+	print('viki say:' , viki_word_count,'貼圖:',viki_sticker_count,'圖片:',viki_image_count)
 
 #寫入檔案
 def write_file(filename,lines):
