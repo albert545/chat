dea744a047105bb0b9e7c4ba71c4ea68224b932e
Albert Liao <pulieden@gmail.com>
2018-03-04 09:41:48 +0800

    讀取檔案 function and main

---
+++ r1.py
@@ -0,0 +1,14 @@
+
+#讀取檔案
+def read_file(filename):
+	lines = []
+	with open(filename,'r',encoding = 'utf-8-sig') as f: #uth-8-sig 去掉先前字元
+		for line in f:
+			lines.append(line.strip())
+	return lines
+
+def main():
+	lines = read_file('input.txt')
+	print(lines)
+
+main()
